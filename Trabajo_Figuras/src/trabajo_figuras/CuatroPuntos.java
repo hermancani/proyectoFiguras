15b6e71f7a6e51dd5522ef2da8d1383d2948e42e
sebamartinez10 <sebamartinezerree@gmail.com>
2017-11-13 18:09:42 -0300

    se crearon atributo

--- Trabajo_Figuras/src/trabajo_figuras/CuatroPuntos.java
+++ Trabajo_Figuras/src/trabajo_figuras/CuatroPuntos.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package trabajo_figuras;
 
-/**
- *
- * @author sebamartinez10
- */
+package trabajo_figuras;
+   
 public class CuatroPuntos {
-    
+    private double x;
+    private double y;
+
 }
